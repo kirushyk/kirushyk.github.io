5a1548719fd06c823f7cf6ae7c7defecc15dce12
Kyrylo Polezhaiev <kirushyk@gmail.com>
2020-06-17 17:09:06 +0300

    ui: Add pages

---
+++ ui.js
@@ -0,0 +1,6 @@
+function setPage(page) {
+    for (var i = 0; i <= 2; i++) {
+        document.getElementById('page' + i).setAttribute('style', page === i ? 'display:block' : 'display:none')
+        document.getElementById('li' + i).setAttribute('class', page === i ? 'nav-item active' : 'nav-item')
+    }
+}
