f87f2574d4a6425532674c1e35acf884d94f341b
Kyrylo Polezhaiev <kirushyk@gmail.com>
2021-01-03 03:54:54 +0200

    Have 1-page look

--- ui.js
+++ ui.js
@@ -1,5 +1,5 @@
 function setPage(page) {
-    for (var i = 0; i <= 2; i++) {
+    for (var i = 0; i <= 3; i++) {
         document.getElementById('page' + i).setAttribute('style', page === i ? 'display:block' : 'display:none')
         document.getElementById('li' + i).setAttribute('class', page === i ? 'nav-item active' : 'nav-item')
     }
